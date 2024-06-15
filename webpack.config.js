1c1e1b634fdd8fe78609ae73505aa07c7cfe7fbd
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-06-15 10:38:17 +0800

    initial set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 // This file defines how Webpack should process, bundle, and optimize the assets and modules in your project.
 
 // Loaders: css and stylesheets
-// plugins: HtmlWebpackPlugin
+// plugins: HtmlWebpackPlugin,CleanWebpackPlugin(cleans dist folder bf each build)
 // devtools: inline-source-map & devserver & barbell & linter & prettier
 
 const path = require('path'); // Imports the path module from Node.js, which provides functions for working with file and directory paths. rerquire for webpack.config file, import/export for JS file
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Import CleanWebpackPlugin
+
 
 module.exports = {
   mode: 'development',
   entry: './src/assets/index/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.js',  // Name of the output bundle
     path: path.resolve(__dirname, 'dist'),
     // path.resolve is a method provided by Node.js's path module. It's used to resolve/combine a sequence of path segments into an absolute path.
   },
@@ -47,6 +49,9 @@ module.exports = {
       // If we had our own dist/index.html then it would be overwritten! In order to preserve our own HTML,
       // we provide webpack with a template html file in src, then specify the o/p html filename
     }),
+
+    new CleanWebpackPlugin(),  // Clean the output directory before each build
+
   ],
 
   devtool: 'inline-source-map',
@@ -54,11 +59,8 @@ module.exports = {
   devServer: {
     // devServer option allows you to configure the behavior of the webpack development server
     static: './dist',
-    watchFiles: ['src/**/*'], // watch all the subdirectories & files in source directory
+    watchFiles: ['src/index.html'], // watch all the subdirectories & files in source directory
   },
 
-  optimization: {
-    // added only when there is more than one entrypoint on a single HTML page. W/o this there will be issues
-    runtimeChunk: 'single',
-  },
+  
 };
