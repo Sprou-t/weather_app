c13f4df26104a87ef65b19432d17f6b6b595ae7d
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-06-17 01:20:27 +0800

    added basic styling for the default page

--- src/assets/index/index.js
+++ src/assets/index/index.js
@@ -1,23 +1,41 @@
 import './index.css';
 import './reset.css';
+import logoIcon from '../photo/cloud-clock-outline.svg';
 
+const body = document.querySelector('body');
 // function creates the webage layout
 const createLayout = (() => {
 	const header = document.createElement('div');
+	header.classList.add('header');
 	const logoDiv = document.createElement('div');
+	logoDiv.classList.add('logoDiv');
 	const logoImg = document.createElement('img');
-	logoImg.src = '../photo/cloud-clock-outline.svg';
+	logoImg.src = logoIcon;
 	const logoName = document.createElement('p');
 	logoName.textContent = 'Weather.You';
+	const searchDiv = document.createElement('div');
+	searchDiv.classList.add('searchDiv');
 	const searchBar = document.createElement('input');
 	searchBar.placeholder = 'Enter your location';
 	const searchBtn = document.createElement('button');
 	searchBtn.textContent = 'Search';
+	searchDiv.append(searchBar, searchBtn);
+	logoDiv.append(logoImg, logoName);
+	header.append(logoDiv, searchDiv);
 
-	const bodyDIv = document.createElement('div');
+	const bodyDiv = document.createElement('div');
+	bodyDiv.classList.add('bodyDiv');
 	const locationText = document.createElement('p');
+	locationText.textContent = 'Kandy,Sri Lanka';
+	locationText.classList.add('locationText');
 	const dateTimeDiv = document.createElement('div');
-	const datePara = document.createElement('p');
+	dateTimeDiv.classList.add('dateTimeDiv');
+	const datePara = document.createElement('div');
+	datePara.textContent = 'Monday, 20th September 2021';
 	const timePara = document.createElement('p');
+	timePara.textContent = '12:00 PM';
+	dateTimeDiv.append(datePara, timePara);
+	bodyDiv.append(locationText, dateTimeDiv);
 
+	body.append(header, bodyDiv);
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -9,56 +9,58 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Import CleanWebpackPlugin
 
 module.exports = {
-  mode: 'development',
-  entry: './src/assets/index/index.js',
-  output: {
-    filename: 'main.js',
-    // Name of the output bundle
-    path: path.resolve(__dirname, 'dist'),
-    // path.resolve is a method provided by Node.js's path module. It's used to resolve/combine a sequence of path segments into an absolute path.
-  },
+	mode: 'development',
+	entry: './src/assets/index/index.js',
+	output: {
+		filename: 'main.js',
+		// Name of the output bundle
+		path: path.resolve(__dirname, 'dist'),
+		// path.resolve is a method provided by Node.js's path module. It's used to resolve/combine a sequence of path segments into an absolute path.
+	},
 
-  module: {
-    // tranasfor specific files into specific modules to be imported & used
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', { targets: 'defaults' }]],
-          },
-        },
-      },
-    ],
-  },
+	module: {
+		// tranasfor specific files into specific modules to be imported & used
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', { targets: 'defaults' }],
+						],
+					},
+				},
+			},
+		],
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      // If we had our own dist/index.html then it would be overwritten! In order to preserve our own HTML,
-      // we provide webpack with a template html file in src, then specify the o/p html filename
-    }),
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+			// If we had our own dist/index.html then it would be overwritten! In order to preserve our own HTML,
+			// we provide webpack with a template html file in src, then specify the o/p html filename
+		}),
 
-    new CleanWebpackPlugin(),
-    // Clean the output directory before each build
-  ],
+		new CleanWebpackPlugin(),
+		// Clean the output directory before each build
+	],
 
-  devtool: 'inline-source-map',
+	devtool: 'inline-source-map',
 
-  devServer: {
-    // devServer option allows you to configure the behavior of the webpack development server
-    static: './dist',
-    watchFiles: ['src/index.html'], // watch all the subdirectories & files in source directory
-  },
+	devServer: {
+		// devServer option allows you to configure the behavior of the webpack development server
+		static: './dist',
+		watchFiles: ['src/index.html'], // watch all the subdirectories & files in source directory
+	},
 };
