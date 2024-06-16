19532cd8ce69deb1b8b38424cf2f2e4d4be01ddf
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-06-16 23:04:40 +0800

    overrode airbnb's indentation setting from 2 to 4 spacing. Also created basic layout content of webpage without appending yet

--- src/assets/index/index.js
+++ src/assets/index/index.js
@@ -1,2 +1,23 @@
 import './index.css';
 import './reset.css';
+
+// function creates the webage layout
+const createLayout = (() => {
+	const header = document.createElement('div');
+	const logoDiv = document.createElement('div');
+	const logoImg = document.createElement('img');
+	logoImg.src = '../photo/cloud-clock-outline.svg';
+	const logoName = document.createElement('p');
+	logoName.textContent = 'Weather.You';
+	const searchBar = document.createElement('input');
+	searchBar.placeholder = 'Enter your location';
+	const searchBtn = document.createElement('button');
+	searchBtn.textContent = 'Search';
+
+	const bodyDIv = document.createElement('div');
+	const locationText = document.createElement('p');
+	const dateTimeDiv = document.createElement('div');
+	const datePara = document.createElement('p');
+	const timePara = document.createElement('p');
+
+})();
